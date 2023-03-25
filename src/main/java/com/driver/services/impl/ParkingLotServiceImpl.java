933d26e11649a1788e0517954dbf43f0d0ee5c85
Syed Aman Ali <105538460+itsamanali@users.noreply.github.com>
2023-03-25 17:09:00 +0530

    service added

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -1,5 +1,7 @@
 package com.driver.services.impl;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Spot;
 import com.driver.model.SpotType;
 import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.SpotRepository;
@@ -18,12 +20,27 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     SpotRepository spotRepository1;
     @Override
     public ParkingLot addParkingLot(String name, String address) {
-
+        ParkingLot parkingLot = new ParkingLot(name , address);
+        parkingLotRepository1.save(parkingLot);
+        return parkingLot;
     }
 
     @Override
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
-
+        SpotType spotType = SpotType.OTHERS;
+        if (numberOfWheels == 2){
+           spotType = SpotType.TWO_WHEELER;
+        }
+        else if(numberOfWheels == 4){
+            spotType = SpotType.FOUR_WHEELER;
+        }
+        Spot spot = new Spot(spotType , pricePerHour);
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        List<Spot> spotList = parkingLot.getSpotList();
+        spotList.add(spot);
+        parkingLotRepository1.save(parkingLot);
+        spotRepository1.save(spot);
+        return spot;
     }
 
     @Override
